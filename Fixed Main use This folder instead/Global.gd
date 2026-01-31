a025e06485a7b0e2782814073ac8b73e6e6bbac2
cjnetto <cjnetto13@gmail.com>
2026-01-31 14:13:37 +0800

    potion healing
    potions collected from the map can now be used in battle scenes

--- Fixed Main use This folder instead/Global.gd
+++ Fixed Main use This folder instead/Global.gd
@@ -3,6 +3,5 @@ extends Node
 # This script stays alive across the whole game
 var player_hp = 100
 var player_max_hp = 100
-var potion_count = 0  # Your "Items" task lives here
-
+var potion_count = 0
 # You can add other items later, like 'var bombs = 0'
